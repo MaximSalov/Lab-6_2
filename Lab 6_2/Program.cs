758867be99a968bf87c5a0dde231037479879eec
ПОльзователь <@USER436809363>
2021-10-28 15:10:40 +0300

    Добавьте файлы проекта.

---
+++ Lab 6_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string strLow = str.ToLower().Replace(" ","");
+            string reverseStr = "";
+            for (int i = strLow.Length - 1; i >= 0; i--)
+            {
+                reverseStr += strLow[i];
+            }
+            if (strLow == reverseStr)
+                Console.WriteLine("Введенная строка является палиндромом.");
+            else
+                Console.WriteLine("Введенная строка не является палиндромом.");
+            Console.ReadKey();
+        }
+    }
+}
